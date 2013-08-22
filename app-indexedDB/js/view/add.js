fc91eb60d779ecd90f15b94fb0f5555f59b4dc9f
Yong Guan <gyong9@gmail.com>
2013-08-21 21:56:50 -0700

    get chrome active tab if the code is in extension

--- app-indexedDB/js/view/add.js
+++ app-indexedDB/js/view/add.js
@@ -1,11 +1,38 @@
 var bookmarkRepo = require('data/bookmark-repository'),
     tagGroupRepo = require('data/tag-group-repository');
 
+function getActiveTab(op) {
+    console.log(chrome.tabs);
+    if (chrome.tabs) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+            // since only one tab should be active and in the current window at once
+            // the return variable should only have one entry
+            var activeTab = arrayOfTabs[0];
+            op.success(activeTab);
+        });
+    } else {
+        op.failure();
+    }
+}
+
 module.exports = {
     name: 'AddCtrl',
     controller: function($scope, $location) {
 
         tagGroupRepo.loadAllTagsToCache({});
+
+        getActiveTab({
+            success: function (tab) {
+                $scope.url = tab.url;
+                $scope.title = tab.title;
+                $scope.$apply();
+            },
+            failure: function () {
+                $scope.url = window.location.href;
+                $scope.title = window.document.title;
+                $scope.$apply();
+            }
+        });
 
         $scope.getTags = function () {
             return tagGroupRepo.getAllTags();
@@ -27,7 +54,6 @@ module.exports = {
                             console.log(results);
                         }
                     });
-
                 }
             })
         };
